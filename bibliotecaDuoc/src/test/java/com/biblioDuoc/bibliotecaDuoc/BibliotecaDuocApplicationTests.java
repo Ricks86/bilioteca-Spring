ff896a092b795b6de89111ef56990e16bc4748c5
Albedo-AB <di.aravenab@duocuc.cl>
2026-03-19 16:10:58 -0300

    primer avance

---
+++ bibliotecaDuoc/src/test/java/com/biblioDuoc/bibliotecaDuoc/BibliotecaDuocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioDuoc.bibliotecaDuoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDuocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
